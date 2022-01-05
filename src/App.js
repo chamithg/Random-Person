42d6ef58adf819f016682de228ec61e9332bee3a
gamagegit <cgamage92@yahoo.com>
2022-01-05 11:48:24 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from "react-icons/fa";
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
+
+  const fetchPerson = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const newPerson = data.results[0];
+
+      if (newPerson) {
+        const randomPerson = {
+          image: newPerson.picture.large,
+          phone: newPerson.cell,
+          name: `${newPerson.name.first} ${newPerson.name.last}`,
+          email: newPerson.email,
+          age: newPerson.dob.age,
+          street: `${newPerson.location.street.number} ${newPerson.location.street.name}`,
+          password: newPerson.login.password,
+        };
+
+        setPerson(randomPerson);
+        setValue(randomPerson.name);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchPerson();
+  }, []);
+
+  const handleValue = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("icon")) {
+      const newValue = e.target.dataset.label;
+      setTitle(newValue);
+      setValue(person[newValue]);
+    }
+  };
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-mg"
+          />
+          <p className="user-title">my {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}>
+              <FaUser />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}>
+              <FaEnvelopeOpen />
+            </button>
+            <button
+              className="icon"
+              data-label="age"
+              onMouseOver={(e) => handleValue(e)}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}>
+              <FaMap />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}>
+              <FaPhone />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}>
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" onClick={fetchPerson}>
+            {loading ? "loading..." : "random user"}
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
